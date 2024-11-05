40cc86099c029d6e155475f7de6976c121eedb6d
nttt2004 <124849254+nttt2004@users.noreply.github.com>
2024-11-05 20:33:50 +0700

    Sửa xíu layout

--- ver1/src/Layout/index.tsx
+++ ver1/src/Layout/index.tsx
@@ -11,11 +11,13 @@ const Layout: React.FC<Props> = ({ children, className }) => {
         className || ""
       }`}
     >
-      <Menu children={undefined} />
-      <article className="flex flex-col items-center gap-y-[117px] min-w-0 mb-[61px] mt-[40px] ml-8 min-h-[1000px]">
-        {/* Main content area */}
-        {children}
-      </article>
+      <div className="flex">
+        <Menu className="flex-1" children={undefined} />
+        <article className="flex flex-col flex-4 items-center gap-y-[117px] min-w-0 mb-[61px] mt-[40px] ml-8 min-h-[1000px]">
+          {/* Main content area */}
+          {children}
+        </article>
+      </div>
     </section>
   );
 };
